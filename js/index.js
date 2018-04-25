399c18006454f83c03e5d76e6f790c7bd97d36c9
David Lukes <dafydd.lukes@gmail.com>
2018-04-25 13:37:33 +0100

    wip

--- js/index.js
+++ js/index.js
@@ -11,41 +11,88 @@ const neg_feedback = new Set([
   "Better luck next time!",
 ]);
 
+// const NextButton = Vue.component("button-next", {
+//   template: `
+// <button @click="goToNext" id="btn-next" type="button" class="btn btn-sm btn-outline-secondary">Next >></button>
+// `,
+//   props: ["fragment"],
+//   methods: {
+//     goToNext() {
+//       this.$router.push({ path: "/", params: { page: 0, slide: 0, fragment: this.fragment }});
+//       console.log("going to next");
+//       console.log(this.fragment);
+//     }
+//   }
+// });
+
+function hasNext(coll, idx) {
+  return idx + 1 < coll.length;
+}
+
 const Presentation = Vue.component("presentation", {
   template: `
 <div>
 <p v-for="f in fragments">{{ f }}</p>
+<router-link :to="{ name: 'main', params: { page: next_page, slide: next_slide, fragment: next_fragment }}">Next</router-link>
 </div>
 `,
   data() {
     return {
       // currently displayed fragments
-      fragments: []
+      fragments: [],
+      next_page: 0,
+      next_slide: 0,
+      next_fragment: 0
     };
   },
   methods: {
-    setFragments(params, data) {
-      let page = params.page;
-      let slide = params.slide;
-      let fragment = params.fragment;
-      let new_fragments = pages[page].slides[slide].fragments.slice(0, fragment);
-      this.fragments = new_fragments;
+    updateFragment(params) {
+      let page = parseInt(params.page);
+      let slide = parseInt(params.slide);
+      let fragment = parseInt(params.fragment);
+      let slides = pages[page].slides;
+      let fragments = slides[slide].fragments;
+      let display_fragments = fragments.slice(0, fragment + 1).map(e => e.content);
+      let last_fragment = display_fragments[fragment];
+      if (last_fragment.hasOwnProperty("enter")) {
+        last_fragment.enter();
+      }
+      let canvas = $("#msCanvas");
+      if (last_fragment.hasOwnProperty("next")) {
+        console.log("ADDING CANVAS CLICK HANDLER");
+        canvas.click(last_fragment.next);
+      } else {
+        // TODO: remove possible existing click handlers on canvas
+        // TODO: hide next router-link
+      }
+      this.fragments = display_fragments;
+      // set up link to next fragment
+      if (hasNext(fragments, fragment)) {
+        this.next_fragment = fragment + 1;
+      } else if (hasNext(slides, slide)) {
+        this.next_slide = slide + 1;
+        this.next_fragment = 0;
+      } else if (hasNext(pages, page)) {
+        this.next_page = page + 1;
+        this.next_slide = 0;
+        this.next_fragment = 0;
+      }
     }
   },
   created() {
     let params = this.$route.params;
-    this.setFragments(params, this);
+    this.updateFragment(params);
   },
   beforeRouteUpdate(to, from, next) {
     let params = to.params;
-    this.setFragments(params, this);
+    this.updateFragment(params);
     next();
   }
 });
 
 const routes = [
-  { path: "/:page/:slide/:fragment", component: Presentation },
-  { path: "/", redirect: "/0/0/1" }
+  { name: "main", path: "/:page/:slide/:fragment", component: Presentation },
+  { path: "/", redirect: "/0/0/0" }
 ];
 const router = new VueRouter({
   routes
--- js/presentation.js
+++ js/presentation.js
@@ -1,3 +1,8 @@
+const zones = {
+  z1: {
+    points: [{"x":464,"y":679},{"x":736,"y":679},{"x":733,"y":729},{"x":470,"y":739}]
+  }
+};
 const pages = [
   // a presentation consists of pages, corresponding to the scanned pages of a manuscript
   {
@@ -6,19 +11,46 @@ const pages = [
       {
         // slides are divided into fragments
         fragments: [
-          "Hey there!",
-          "This is a really nice interactive app for exploring manuscripts.",
-          "Feel free to poke around :)",
-          "Hey there!",
-          "This is a really nice interactive app for exploring manuscripts.",
-          "Feel free to poke around :)",
-          "Hey there!",
-          "This is a really nice interactive app for exploring manuscripts.",
-          "Feel free to poke around :)",
+          {
+            content: `
+Hey there!
+`
+          },
+          {
+            content: `
+Hey there number two!
+`,
+            enter: function() {
+              // zoom onto
+              focusOn(zones.z1);
+            },
+            next: function(event) {
+              // custom next action (e.g. locate an element in manuscript)
+              locatePolygon(zones.z1, event, focusOn);
+              console.log(event);
+            }
+          },
+          {
+            content: `
+This is a really nice interactive app for exploring manuscripts.
+`
+          },
+        ]
+      },
+      {
+        fragments: [
+          {
+            content: `
+Feel free to poke around :)
+`
+          },
+          {
+            content: `
+Really, do!
+`
+          },
         ]
       }
     ]
   },
-  {},
-  {},
 ];
